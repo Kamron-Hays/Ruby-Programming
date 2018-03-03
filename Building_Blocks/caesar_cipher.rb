47d9cac1b9dc9fa33da15a63ed14621bc0c33c00
Kamron Hays <kamron.hays@live.com>
2018-03-03 07:27:25 -0500

    Completed exercises

---
+++ Building_Blocks/caesar_cipher.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+=begin
+In cryptography, a Caesar cipher, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.
+=end
+
+Lower = ('a'..'z').to_a
+Upper = ('A'..'Z').to_a
+
+def caesar_cipher(input, shift)
+  result = ""
+
+  # iterate over each character in the string
+  input.each_char do |c|
+    index = Lower.index(c)
+    if index != nil
+      result += Lower[(index + shift) % 26]
+    else
+      index = Upper.index(c)
+      if ( index != nil )
+        result += Upper[(index + shift) % 26]
+      else
+        result += c
+      end
+    end
+  end
+  
+  result
+end
+
+puts caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 5) == "Bmfy f xywnsl!"
---
+++ Building_Blocks/stock_picker.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+=begin
+Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
+
+  > stock_picker([17,3,6,9,15,8,6,1,10])
+  => [1,4]  # for a profit of $15 - $3 == $12
+Quick Tips:
+
+You need to buy before you can sell
+Pay attention to edge cases like when the lowest day is the last day or the highest day is the first day.
+=end
+
+def stock_picker(prices)
+  if prices.length < 2
+    return nil
+  end
+
+  days = nil
+  max_profit = 0
+
+  prices.each_index do |i|
+    prices.each_index do |j|
+      next unless j > i
+      profit = prices[j] - prices[i]
+
+      if profit > max_profit
+        max_profit = profit
+        days = [i, j]
+      end
+    end
+  end
+
+  days
+end
+
+puts stock_picker([17,3,6,9,15,8,6,1,10]) == [1,4]

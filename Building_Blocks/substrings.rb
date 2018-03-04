f466b85922f99b633bbfd13b27083d0919bb56a5
Kamron Hays <kamron.hays@live.com>
2018-03-04 08:21:27 -0500

    Added substrings exercise

---
+++ Building_Blocks/substrings.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+=begin
+Implement a method #substrings that takes a word as the first argument and then
+an array of valid substrings (your dictionary) as the second argument. It
+should return a hash listing each substring (case insensitive) that was found
+in the original string and how many times it was found.
+
+  > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  > substrings("below", dictionary)
+  => { "below" => 1, "low" => 1 }
+Next, make sure your method can handle multiple words:
+
+  > substrings("Howdy partner, sit down! How's it going?", dictionary)
+  => { "down" => 1, "how" => 2, "howdy" => 1,"go" => 1, "going" => 1, "it" => 2, "i" => 3, "own" => 1,"part" => 1,"partner" => 1,"sit" => 1 }
+=end
+
+def substrings(text, list)
+  result = {}
+  list.each do |substring|
+    text.split(/[\s,!?]+/).each do |word|
+      if word.downcase.include? substring
+        if result[substring] == nil
+          result[substring] = 1
+        else
+          result[substring] += 1
+        end
+      end
+    end
+  end
+
+  result
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary).to_s == '{"down"=>1, "go"=>1, "going"=>1, "how"=>2, "howdy"=>1, "it"=>2, "i"=>3, "own"=>1, "part"=>1, "partner"=>1, "sit"=>1}'

9a45409f295d26d5bea27770b67de9484e6c5a03
Kamron Hays <kamron.hays@live.com>
2018-03-25 15:34:55 -0400

    Added my_count Enumerable

--- Advanced_Building_Blocks/my_enumerables.rb
+++ Advanced_Building_Blocks/my_enumerables.rb
@@ -16,30 +16,48 @@ module Enumerable
   def my_select
     return to_enum(__method__) unless block_given?
     arr = []
-  	self.my_each { |item| arr << item if yield(item) }
-  	arr
+    self.my_each { |item| arr << item if yield(item) }
+    arr
   end
 
   def my_all?
-    return true unless block_given?
-    self.my_each do |item|
-      return false unless yield(item)
+    if block_given?
+      self.my_each { |item| return false if !yield(item) }
+    else
+      self.my_each { |item| return false if !item }
     end
     true
   end
 
-  def my_any?
-    return true unless block_given?
-    self.my_each do |item|
-      return true if yield(item)
-    end
-    false
-  end
-
   def my_none?
+    if block_given?
+      self.my_each do |item|
+        return false if yield(item)
+      end
+    else
+      self.my_each do |item|
+        return false if item
+      end
+    end
+    true
   end
 
-  def my_count
+  def my_any?(&block)
+    !self.my_none?(&block)
+  end
+  
+  def my_count(item=nil)
+    if block_given?
+      count=0
+		  self.my_each { |x| count += 1 if yield(x) }
+      return count
+    else
+      if item == nil
+        return self.size
+      else
+        return self.my_count { |x| x == item }
+      end
+    end
   end
 
   def my_map
@@ -65,6 +83,21 @@ p [1,2,3,4,5,6].my_select { |item| item%2 == 0 } == [2,4,6]
 
 p [1,2,3].my_all? { |item| item < 4 } == true
 p [1,2,3].my_all? { |item| item < 3 } == false
+p [0,1,2].my_all? == true
+p [nil, true, 99].my_all? == false
 
 p [1,2,3].my_any? { |item| item == 2 } == true
 p [1,2,3].my_any? { |item| item == 4 } == false
+p [nil, false, 99].my_any? == true
+p [false, nil, false].my_any? == false
+
+p [1,2,3].my_none? { |item| item == 2 } == false
+p [1,2,3].my_none? { |item| item == 4 } == true
+p [].my_none? == true
+p [nil].my_none? == true
+p [nil, false].my_none?
+
+p [1,2,4,2].my_count == 4
+p [1,2,4,2].my_count(2) == 2
+p [1,2,4,2].my_count(3) == 0
+p [1,2,4,2].my_count { |x| x%2 == 0 } == 3

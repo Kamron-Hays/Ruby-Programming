39dbf56b171c47b433aa9715543c87584e53ee6c
Kamron Hays <kamron.hays@live.com>
2018-04-29 21:45:56 -0400

    Added shortest path determination

--- Data_Structures/knights_travails.rb
+++ Data_Structures/knights_travails.rb
@@ -7,7 +7,7 @@
 # https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/describing-graphs.
 
 class Knight
-  attr_accessor :position, :neighbors
+  attr_accessor :position, :neighbors, :previous
 
   # Rules that govern relative movement for a Knight.
   RULES = [[1,2], [-1,2], [1,-2], [-1,-2],
@@ -17,6 +17,7 @@ class Knight
   def initialize(position)
     @position = position
     @neighbors = []
+    @previous = nil # where this knight came from - used for finding shortest path between two positions
   end
 
   # Returns an array of all possible moves from this Knight's current position.
@@ -31,13 +32,13 @@ class Knight
   end
 
   def to_s
-    "Knight@[#{@position[0]},#{@position[1]}]"
+    "N#{@position[0]},#{@position[1]}"
   end
   alias :inspect :to_s
 end
 
-# The Board is a Graph that contains a Knight at every position, along with
-# the path of how it got there.
+# The Board is a Graph that contains a Knight at every position (vertices),
+# along with all possible positions to which it can move (edges).
 class Board
   attr_accessor :knights
 
@@ -65,15 +66,61 @@ class Board
     end
   end
 
+  # Returns true if a move to the specified position is legal. Since the
+  # movements of a single piece are being modeled, a move to any position
+  # is legal, as long as it's on the board.
   def legal?(pos)
     x,y = pos
     x >= 0 && x <= 7 && y >= 0 && y <= 7
   end
+
+  # Returns a list of all the positions for the shortest route between the
+  # specified start and finish positions (an x,y coordinate). A breadth-first
+  # traversal is performed on the board (a graph). Since the edges in this
+  # graph are unweighted (the "distance" is the same between all vertices),
+  # a BFS traversal will automatically find the closest path, because vertices
+  # are visited in order of their distance from the starting point. All that
+  # needs to be done is save the previous node from the currently visited
+  # node. Once the specified finish positon is found, the path can be built
+  # by traversing from finish to start, using the "previous node" links.
+  def knight_moves(start, finish)
+    # Clear all the "previous" links
+    @knights.each do |pos, knight|
+      knight.previous = nil
+    end
+
+    # Perform the BFS traversal, saving the "previous node" links.
+    queue = [ @knights[start] ]
+    until queue.empty?
+      knight = queue.shift
+      break if knight.position == finish
+
+      knight.neighbors.each do |neighbor|
+        if neighbor.previous == nil
+          # This node hasn't been visited yet.
+          neighbor.previous = knight
+          queue << neighbor
+        end
+      end
+    end
+
+    # Now follow the "previous node" links to reconstruct the path.
+    path = []
+    vertex = @knights[finish]
+    loop do
+      path.unshift(vertex.position)
+      break if vertex.position == start
+      vertex = vertex.previous
+    end
+    path
+  end
 end
 
-# https://stackoverflow.com/questions/8379785/how-does-a-breadth-first-search-work-when-looking-for-shortest-path
 if __FILE__ == $0
   # this will only run if the script was the main, not load'd or require'd
 
   board = Board.new
+  p board.knight_moves([0,0], [1,2]) == [[0,0], [1,2]]
+  p board.knight_moves([0,0], [3,3]) == [[0,0], [1,2], [3,3]]
+  p board.knight_moves([3,3], [0,0]) == [[3,3], [2,1], [0,0]]
 end
